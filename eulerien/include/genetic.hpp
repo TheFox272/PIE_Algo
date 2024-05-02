904cb6fd6ce33268b2b32bfac79f1cb881e81a67
TheFox272 <fabien.girard27@gmail.com>
2024-05-02 22:12:07 +0200

    fixes hierolzer

--- eulerien/include/genetic.hpp
+++ eulerien/include/genetic.hpp
@@ -15,10 +15,10 @@ int evaluer(const GrapheAugmente g);
 
 std::vector<GrapheAugmente> init_population(const Graphe g, const int taille_pop);
 
-std::vector<GrapheAugmente> selection(const std::vector<GrapheAugmente>& pop, const int taille_pop, const double taux_selection);
+std::vector<GrapheAugmente> selection(const std::vector<GrapheAugmente>& pop, const int taille_selection);
 
-std::vector<GrapheAugmente> reproduction(const std::vector<GrapheAugmente>& pop, const int taille_pop, const double taux_reproduction, const double taux_mutation);
+std::vector<GrapheAugmente> reproduction(const std::vector<GrapheAugmente>& pop, const int taille_pop, const int taille_evolution, const double taux_mutation);
 
-std::vector<GrapheAugmente> evolution(const Graphe g, const int taille_pop, const int nb_gen, const double taux_reproduction, const double taux_mutation);
+std::vector<GrapheAugmente> evolution(const Graphe g, const int taille_pop, const int nb_gen, const double taux_selection, const double taux_mutation);
 
-GrapheAugmente generer_graphe_eulerien(const Graphe g, const int taille_pop, const int nb_gen, const double taux_reproduction, const double taux_mutation);
+GrapheAugmente generer_graphe_eulerien(const Graphe g, const int taille_pop, const int nb_gen, const double taux_selection, const double taux_mutation);
